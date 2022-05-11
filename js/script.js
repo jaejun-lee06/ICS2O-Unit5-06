3c224dcaac153ce491531a0ff92a07f3a91b429f
Jaejun Lee <99355321+jaejun-lee06@users.noreply.github.com>
2022-05-11 17:10:19 +0000

    done

--- js/script.js
+++ js/script.js
@@ -2,29 +2,39 @@
 // Created on: May 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict";
 
 /**
- * Calculates the product of two numbers the user provides.
+ * This function does multiplication.
  */
-function myButtonClicked() {
-  const integerOne = parseInt(document.getElementById("integer1").value)
-  const integerTwo = parseInt(document.getElementById("integer2").value)
+function myButtonClicked () {
+
+  const integer1 = parseInt(document.getElementById("integer1").value)
+	const integer2 = parseInt(document.getElementById("integer2").value)
+  var addedInteger = 0
   var result = 0
 
-  if (integerOne != 0 && integerTwo >= 0) {
-    while (integerTwo > 0) {
-      // Calculation for + or - times + numbers
-      result = result + integerOne
-      integerTwo = integerTwo - 1
+  if (integer1 > 0 && integer2 > 0) {
+    while (addedInteger < integer2) {
+      addedInteger = addedInteger + 1;
+      result = result + integer1;
+    }
+  } else if (integer1 < 0 && integer2 < 0) {
+    while (addedInteger > integer2) {
+      addedInteger = addedInteger - 1;
+      result = result - integer1;
+    }
+  } else if (integer1 > 0 && integer2 < 0) {
+    while (addedInteger > integer2) {
+      addedInteger = addedInteger - 1;
+      result = result - integer1;
+    }
+  } else {
+    while (addedInteger < integer2) {
+      addedInteger = addedInteger + 1;
+      result = result + integer1;
     }
   }
-  else if (integerTwo < 0) {
-    while (integerTwo != 0) {
-      // Calculation for + or - times - numbers
-      result = result - integerOne
-      integerTwo = integerTwo + 1
-    }
-  }
-  document.getElementById("answers").innerHTML = "The product of these numbers is " + result
+  
+  document.getElementById("answers").innerHTML = "The answer is: " + integer1 + " x " + addedInteger + " = " + result;
 }
